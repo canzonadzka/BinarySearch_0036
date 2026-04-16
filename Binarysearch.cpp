fef5d29e0f86080f58b02f10dea1371f368cb900
canzonadzka <canzonadzka@gmail.com>
2026-04-16 15:23:41 +0700

    menyelesaikan void display

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -54,3 +54,18 @@ void input()
 }
 
 void display()
+{
+    cout << "\n============================================\n";
+    cout << "  Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
